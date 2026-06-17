aac90b5913f486610dc4a9ae2b4eb42569551158
hardik211005 <hardikdhameeja2105@gmail.com>
2026-06-17 23:41:41 +0530

    implement rank tracking API and integrate with frontend; refactor keyword handling in RankTracker

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import connectDB from './config/db.js';
 import authRouter from './routes/authRoutes.js';
+import rankRouter from './routes/rankRoutes.js';
 
 connectDB();
 const app = express();
@@ -17,6 +18,7 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 app.use('/api/auth', authRouter);
+app.use('/api/rank', rankRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
--- server/services/rankTrackerService.js
+++ server/services/rankTrackerService.js
@@ -1,6 +1,7 @@
 import { chromium } from "playwright-core";
 import Browserbase from "@browserbasehq/sdk";
 
+
 const bb = new Browserbase({
   apiKey: process.env.BROWSERBASE_API_KEY,
 });
