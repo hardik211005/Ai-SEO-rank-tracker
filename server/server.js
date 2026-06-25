e95092bc2e474c4c1021da110f48755fd27589b6
hardik211005 <hardikdhameeja2105@gmail.com>
2026-06-25 14:46:06 +0530

    feat: remove server listen logic to export app for testing

--- server/server.js
+++ server/server.js
@@ -27,6 +27,4 @@ app.use('/api/analysis', analysisRouter)
 startRankTrackingCron()
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export default app;
