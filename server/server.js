2acac3322aef5b6954ce46815bc7aef753e4d350
hardik211005 <hardikdhameeja2105@gmail.com>
2026-06-25 14:53:36 +0530

    feat: add custom CORS middleware for enhanced request handling

--- server/server.js
+++ server/server.js
@@ -10,6 +10,17 @@ import { startRankTrackingCron } from './cron/rankTrackingCron.js';
 connectDB();
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://rankpiloyyt.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
+
 app.use(cors({
     origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://rankpiloyyt.vercel.app"],
     credentials: true,
